83f3288fc2f38994e9d2fa526f7f5d398c1a9e2c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-19 16:40:51 -0600

    betty and case for null node

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,12 +2,12 @@
 /**
  * binary_tree_is_leaf - schecks if node is a leaf
  * @node: node to check
- * 
+ *
  * Return: 1 if leaf and 0 if not
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->left == NULL && node->right == NULL)
+	if (node && node->left == NULL && node->right == NULL)
 		return (1);
 	return (0);
 }
