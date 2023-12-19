e2d4cb37c39a290968f12d305e67e620b545b01c
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-18 19:19:38 -0600

    using task 0 to make new node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,19 +10,16 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = NULL;
 
-	new = malloc(sizeof(binary_tree_t));
+	new = binary_tree_node(parent, value);
 	if (new == NULL)
 		return (NULL);
-	new->n = value;
-	new->parent = parent;
 
-	if (parent->left != NULL)
+	if( parent->left != NULL)
 	{
 		new->left = parent->left;
 		parent->left->parent = new;
 	}
 
 	parent->left = new;
-
 	return (new);
 }
