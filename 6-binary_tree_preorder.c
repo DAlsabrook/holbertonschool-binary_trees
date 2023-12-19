21185eb9ac9d6de48c747db330ada5b174a820fa
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-19 17:21:26 -0600

    first attempt at task 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - searches and processes each node
+ * @tree: root of tree to search
+ * @func: funciton pointer to use and pass node value
+ *
+ * Return: void
+*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	binary_tree_t *current = tree;
+
+	if (tree == NULL)
+		return;
+	func(current->n);
+	binary_tree_preorder(current->left, func);
+	binary_tree_preorder(current->right, func);
+}
