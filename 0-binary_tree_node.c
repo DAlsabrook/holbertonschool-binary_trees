e81d755aad9a5e4e7c673ff4ff796af1f016cf66
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-18 18:29:19 -0600

    conditional jump?

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,7 +3,7 @@
  * binary_tree_node - creates new node
  * @parent: parent node
  * @value: value to put in node
- * 
+ *
  * Return: pointer to new node or NULL on failure
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
