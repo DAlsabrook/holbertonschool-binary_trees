52a7299b4443f8e0c8677408a6fcc8c6f7471cf5
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-20 14:25:28 -0600

    back to 0

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -32,7 +32,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	/*check leaf node parents height*/
 	if (tree->left == NULL && tree_height(tree->parent) == 0)
 		return (1);
-	return (1);
+	return (0);
 }
 
 /**
