d6090aaa9b9b7d207d0dd1483ca4ca3fdebd27ee
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-20 11:31:33 -0600

    Forgot to add to header file again

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,8 +11,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	l = binary_tree_size(tree->left);
-	r = binary_tree_size(tree->right);
+	l = binary_tree_leaves(tree->left);
+	r = binary_tree_leaves(tree->right);
 
 	if (tree->left == NULL && tree->right == NULL)
 	{
