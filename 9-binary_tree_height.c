e16c9630fbc1f32928838b61b1b4f0a52a415ef3
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-19 19:27:10 -0600

    return 0 on NULL root

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t l = -1, r = -1;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 	if (tree->left != NULL)
 		l = binary_tree_height(tree->left);
 	if (tree->right != NULL)
