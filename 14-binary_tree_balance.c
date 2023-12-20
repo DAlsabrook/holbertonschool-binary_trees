482db97032081497a2001f80e1209289784d9336
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-20 12:35:52 -0600

    moving if statement for NULL case

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,11 +7,12 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height = height(tree->left);
-	int right_height = height(tree->right);
+	int left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
+	left_height = height(tree->left);
+	right_height = height(tree->right);
 	return (left_height - right_height);
 }
 /**
