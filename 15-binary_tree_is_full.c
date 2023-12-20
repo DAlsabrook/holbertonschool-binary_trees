11dd1981f60a253add400fcc2d6771ad7af01dc3
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-20 13:33:50 -0600

    rework task 15 with loads of ifs

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,12 +7,26 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	/*1 if full. 0 if not*/
+	int res = 1;
 	if (tree == NULL)
 		return (0);
 	if ((tree->left == NULL && tree->right != NULL) ||
 	 (tree->right == NULL && tree->left != NULL))
 	{
-		return (1);
+		return (0);
 	}
-	return (0);
+	if (res == 1 && tree->left != NULL)
+	{
+		res = binary_tree_full(tree->left);
+		if (res == 0)
+			return (0);
+	}
+	if (res == 1 && tree->right != NULL)
+	{
+		res = binary_tree_full(tree->right);
+		if (res == 0)
+			return (0);
+	}
+	return (1);
 }
