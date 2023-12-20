fc42802560b506785cf6fde48de5bed458e36a4a
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-20 12:54:33 -0600

    added if statments to handle seg fault

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,14 +7,18 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	int left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
-	left_height = height(tree->left);
-	right_height = height(tree->right);
-	left_height += 1;
-	right_height += 1;
+	if (tree->left != NULL)
+		left_height = height(tree->left);
+	if (tree->right != NULL)
+		right_height = height(tree->right);
+	if (left_height > 0)
+		left_height += 1;
+	if (right_height > 0)
+		right_height += 1;
 
 	return (left_height - right_height);
 }
