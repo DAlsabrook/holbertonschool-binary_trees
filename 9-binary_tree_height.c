06e74651e33d56ca871b4bdd4d21a55259f219bd
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-19 19:19:13 -0600

    added ifs to check for null nodes

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,8 +10,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t l = 0, r = 0;
 
 	if (tree == NULL)
-		return (-1);
-	l = binary_tree_height(tree->left);
-	r = binary_tree_height(tree->right);
+		return (0);
+	
+	if (tree->left != NULL)
+		l = binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		r = binary_tree_height(tree->right);
 	return (l > r ? l + 1 : r + 1);
 }
