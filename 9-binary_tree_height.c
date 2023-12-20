e29b208bc86589d257cfa84cdd8be4c4a0d2c1b1
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-19 18:05:34 -0600

    trying to not count the root

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,5 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+	if (tree->parent == NULL)
+		return (binary_tree_height(tree));
 	return (binary_tree_height(tree->left) + 1);
 }
