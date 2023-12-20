d7babdcfb7a70f628f676ebc3ef3376dcd2ac20f
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-20 10:52:17 -0600

    first attempt at 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - get the depth of a node
+ * @tree: Node to measure dpeth of
+ *
+ * Return depth of node
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->parent == NULL)
+		return (-1);
+	binary_tree_depth(tree->parent + 1);
+}
