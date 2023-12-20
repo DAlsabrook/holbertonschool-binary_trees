c1efeb717a2fa8d8fcb23ec775efcd5dc8cb1540
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-19 19:25:51 -0600

    case for NULL root

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,6 +9,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l = -1, r = -1;
 
+	if (tree == NULL)
+		return (-1);
 	if (tree->left != NULL)
 		l = binary_tree_height(tree->left);
 	if (tree->right != NULL)
