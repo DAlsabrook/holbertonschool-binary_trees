b06129bfc888538a28f4a11ffe75b679054723a6
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-20 11:30:31 -0600

    First attempt at task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - count the leaves of a tree
+ * @tree: tree to use
+ *
+ * Return: number of leaves
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t l = 0, r = 0;
+
+	if (tree == NULL)
+		return (0);
+	l = binary_tree_size(tree->left);
+	r = binary_tree_size(tree->right);
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		l = 1;
+		r = 1;
+	}
+	return (l + r);
+}
