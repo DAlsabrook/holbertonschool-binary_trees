d02abadda95fadaabefc2ea9529207478129ac9e
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-20 11:49:25 -0600

    moving count increment above rcursive calls

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,12 +11,12 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	count += binary_tree_nodes(tree->left);
-	count += binary_tree_nodes(tree->right);
-
 	if (tree->left != NULL || tree->right != NULL)
 	{
 		count = 1;
 	}
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
+
 	return (count);
 }
