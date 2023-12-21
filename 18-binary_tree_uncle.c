887a46fd5a3a879228b1e7169bb2ca2985f072ff
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-21 13:06:33 -0600

    First attempt at 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - find uncle of a current
+ * @node: current to find uncle of
+ *
+ * Return pointer to uncle current
+*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *uncle;
+	binary_tree_t *current = node;
+
+	if (current == NULL || current->parent == NULL || current->parent->parent == NULL)
+		return (NULL);
+	current = node->parent;
+	if (current->parent->left != NULL && current->parent->left == current)
+	{
+		uncle = current->parent->right;
+	}
+	/*Node is parents right*/
+	if (current->parent->right != NULL && current->parent->right == current)
+	{
+		uncle = current->parent->left;
+	}
+	return (uncle);
+}
