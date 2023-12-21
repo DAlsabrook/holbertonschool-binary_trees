d5b8bd686af591bca1b8e4484dfd57f6df045b22
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-21 13:08:04 -0600

    betty issue fixes

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,14 +3,16 @@
  * binary_tree_uncle - find uncle of a current
  * @node: current to find uncle of
  *
- * Return pointer to uncle current
+ * Return: pointer to uncle current
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *uncle;
 	binary_tree_t *current = node;
 
-	if (current == NULL || current->parent == NULL || current->parent->parent == NULL)
+	if (current == NULL ||
+	 current->parent == NULL ||
+	  current->parent->parent == NULL)
 		return (NULL);
 	current = node->parent;
 	if (current->parent->left != NULL && current->parent->left == current)
